5e05442df529aa011d92dacd8148793e34f9b8a0
Timothy Ratliff <timothykratliff@gmail.com>
2017-08-24 12:18:32 -0500

    removed duplicate project in VS

--- Jarvis/Jarvis/Program.cs
+++ Jarvis/Jarvis/Program.cs
@@ -1,25 +1,25 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
 
 namespace Jarvis
 {
-    public class Program
+    class Program
     {
+		private DiscordSocketClient _client;
+
 		public static void Main(string[] args)
 			=> new Program().MainAsync().GetAwaiter().GetResult();
 
-		private DiscordSocketClient _client;
 		public async Task MainAsync()
 		{
 			_client = new DiscordSocketClient();
 
 			_client.Log += Log;
+			_client.MessageReceived += MessageReceived;
 
-			string token = "BD4ZJrAS8F-4pdyCluu_gGydnul6UcKA"; // Remember to keep this private!
+			string token = "abcdefg..."; // Remember to keep this private!
 			await _client.LoginAsync(TokenType.Bot, token);
 			await _client.StartAsync();
 
@@ -27,12 +27,18 @@ namespace Jarvis
 			await Task.Delay(-1);
 		}
 
+		private async Task MessageReceived(SocketMessage message)
+		{
+			if (message.Content == "!ping")
+			{
+				await message.Channel.SendMessageAsync("Pong!");
+			}
+		}
+
 		private Task Log(LogMessage msg)
 		{
 			Console.WriteLine(msg.ToString());
 			return Task.CompletedTask;
 		}
-
-
 	}
 }
