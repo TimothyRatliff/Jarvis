2b9afdc9bfe3a393c4ba34580ccf6f3c84f493a9
Timothy Ratliff <timothykratliff@gmail.com>
2017-08-24 13:04:59 -0500

    create infomodule

---
+++ Jarvis/Jarvis/InfoModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Modules;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace Jarvis
+{
+    public class InfoModule : ModuleBase
+    {
+		// ~say hello -> hello
+		[Command("say"), Summary("Echos a message.")]
+		public async Task Say([Remainder, Summary("The text to echo")] string echo)
+		{
+			// ReplyAsync is a method on ModuleBase
+			await ReplyAsync(echo);
+		}
+	}
+}
--- Jarvis/Jarvis/Program.cs
+++ Jarvis/Jarvis/Program.cs
@@ -1,13 +1,18 @@
 ﻿using System;
 using System.Threading.Tasks;
+using System.Reflection;
 using Discord;
 using Discord.WebSocket;
+using Discord.Commands;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Jarvis
 {
     class Program
     {
+		private CommandService commands;
 		private DiscordSocketClient _client;
+		private IServiceProvider services;
 
 		public static void Main(string[] args)
 			=> new Program().MainAsync().GetAwaiter().GetResult();
@@ -15,11 +20,18 @@ namespace Jarvis
 		public async Task MainAsync()
 		{
 			_client = new DiscordSocketClient();
+			commands = new CommandService();
+
+			services = new ServiceCollection()
+				.BuildServiceProvider();
+			
 
 			_client.Log += Log;
 			_client.MessageReceived += MessageReceived;
 
 			string token = "abcdefg..."; // Remember to keep this private!
+
+			await InstallCommands();
 			await _client.LoginAsync(TokenType.Bot, token);
 			await _client.StartAsync();
 
@@ -27,7 +39,33 @@ namespace Jarvis
 			await Task.Delay(-1);
 		}
 
-		private async Task MessageReceived(SocketMessage message)
+		public async Task InstallCommands()
+		{
+			// Hook the MessageReceived Event into our Command Handler
+			_client.MessageReceived += HandleCommand;
+			// Discover all of the commands in this assembly and load them.
+			await commands.AddModulesAsync(Assembly.GetEntryAssembly());
+		}
+
+		public async Task HandleCommand(SocketMessage messageParam)
+		{
+			// Don't process the command if it was a System Message
+			var message = messageParam as SocketUserMessage;
+			if (message == null) return;
+			// Create a number to track where the prefix ends and the command begins
+			int argPos = 0;
+			// Determine if the message is a command, based on if it starts with '!' or a mention prefix
+			if (!(message.HasCharPrefix('!', ref argPos) || message.HasMentionPrefix(_client.CurrentUser, ref argPos))) return;
+			// Create a Command Context
+			var context = new CommandContext(_client, message);
+			// Execute the command. (result does not indicate a return value, 
+			// rather an object stating if the command executed successfully)
+			var result = await commands.ExecuteAsync(context, argPos, services);
+			if (!result.IsSuccess)
+				await context.Channel.SendMessageAsync(result.ErrorReason);
+		}
+
+	private async Task MessageReceived(SocketMessage message)
 		{
 			if (message.Content == "!ping")
 			{
