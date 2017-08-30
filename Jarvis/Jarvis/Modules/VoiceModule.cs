48792e149cab7788f4ffdade88a56db01006b268
TimothyRatliff <timothykratliff@gmail.com>
2017-08-30 14:33:12 -0500

    uncommit VoiceModule

--- Jarvis/Jarvis/Modules/VoiceModule.cs
+++ Jarvis/Jarvis/Modules/VoiceModule.cs
@@ -15,16 +15,16 @@ namespace Jarvis.Modules
     class VoiceModule : ModuleBase
     {
 
-		//[Command("join")]
-		//public async Task JoinChannel(IVoiceChannel channel = null)
-		//{
-		//	// Get the audio channel
-		//	channel = channel ?? (msg.Author as IGuildUser)?.VoiceChannel;
-		//	if (channel == null) { await msg.Channel.SendMessageAsync("User must be in a voice channel, or a voice channel must be passed as an argument."); return; }
+		[Command("join")]
+		public async Task JoinChannel(IVoiceChannel channel = null)
+		{
+			// Get the audio channel
+			channel = channel ?? (Context.Message.Author as IGuildUser)?.VoiceChannel;
+			if (channel == null) { await Context.Message.Channel.SendMessageAsync("User must be in a voice channel, or a voice channel must be passed as an argument."); return; }
 
-		//	// For the next step with transmitting audio, you would want to pass this Audio Client in to a service.
-		//	var audioClient = await channel.ConnectAsync();
-		//}
+			// For the next step with transmitting audio, you would want to pass this Audio Client in to a service.
+			var audioClient = await channel.ConnectAsync();
+		}
 
 
 
