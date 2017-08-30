760823f6c26b1f3e1f5178cd87110fbc923c3747
TimothyRatliff <timothykratliff@gmail.com>
2017-08-30 14:04:17 -0500

    comment out VoiceModule

--- Jarvis/Jarvis/Modules/VoiceModule.cs
+++ Jarvis/Jarvis/Modules/VoiceModule.cs
@@ -1,10 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Modules;
+using Discord.Commands;
+using Discord.WebSocket;
+using Discord.Audio;
 
 namespace Jarvis.Modules
 {
-    class VoiceModule
+    class VoiceModule : ModuleBase
     {
-    }
+
+		//[Command("join")]
+		//public async Task JoinChannel(IVoiceChannel channel = null)
+		//{
+		//	// Get the audio channel
+		//	channel = channel ?? (msg.Author as IGuildUser)?.VoiceChannel;
+		//	if (channel == null) { await msg.Channel.SendMessageAsync("User must be in a voice channel, or a voice channel must be passed as an argument."); return; }
+
+		//	// For the next step with transmitting audio, you would want to pass this Audio Client in to a service.
+		//	var audioClient = await channel.ConnectAsync();
+		//}
+
+
+
+
+
+
+
+	}
 }
