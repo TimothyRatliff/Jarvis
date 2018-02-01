d086c4956de4c3fdee9023b506cfbec76fce3c50
Timothy Ratliff <timothykratliff@gmail.com>
2018-01-31 18:18:47 -0600

    begin work on role management

--- Jarvis/Jarvis/Modules/AdminModule.cs
+++ Jarvis/Jarvis/Modules/AdminModule.cs
@@ -65,6 +65,71 @@ namespace Jarvis.Modules
 			Console.WriteLine(DateTime.Now.ToString() + "	RoleCount | Guild: " + Context.Guild.Name + " | Channel: " + Context.Channel.Name + "");
 		}
 
+        [RequireUserPermission(GuildPermission.ManageRoles)]
+        [RequireBotPermission(GuildPermission.ManageRoles)]
+        [Command("addrole"), Summary("Creates a new joinable role")]
+        private async Task AddRole([Remainder, Summary("Role name")] String input = null)
+        {
+            if (input.StartsWith("+"))
+            {
+                await Context.Guild.CreateRoleAsync(input);
+                await Context.Channel.SendMessageAsync("Created role: **" + input + "**");
+            }
+            else
+            {
+                await Context.Guild.CreateRoleAsync("+" + input);
+                await Context.Channel.SendMessageAsync("Created role: **" + input + "**");
+            }
+        }
 
-	}
+        [RequireUserPermission(GuildPermission.ManageRoles)]
+        [RequireBotPermission(GuildPermission.ManageRoles)]
+        [Command("deleterole"), Summary("Deletes a role")]
+        private async Task DeleteRole([Remainder, Summary("Role name")] String input = null)
+        {
+            var role = Context.Guild.Roles.FirstOrDefault(x => x.Name.ToString() == input);
+            await role.DeleteAsync();
+            await Context.Channel.SendMessageAsync("Deleted role: **" + input + "**");
+        }
+
+
+        [RequireBotPermission(GuildPermission.ManageRoles)]
+        [Command("join"), Summary("Joins a joinable role")]
+        private async Task Join([Remainder, Summary("Role name")] String input = null)
+        {
+            if (input.StartsWith("+"))
+            {
+                var role = Context.Guild.Roles.FirstOrDefault(x => x.Name.ToString() == input);
+
+                if (role != null)
+                {
+                    SocketGuildUser user = Context.User as SocketGuildUser;
+
+                    if (user.Roles.Contains(role))
+                    {
+                        await Context.Channel.SendMessageAsync(user.Mention + " already has the role **" + role.ToString() + "**!");
+                    }
+                    else
+                    {
+                        await user.AddRoleAsync(role);
+                        await Context.Channel.SendMessageAsync("Successfully gave " + user.Mention + " **" + role.ToString() + "**");
+                    }
+                }
+            }
+            else
+                await Context.Channel.SendMessageAsync("Unable to find role: **" + input + "**");
+
+        }
+
+        [RequireUserPermission(GuildPermission.ManageRoles)]
+        [RequireBotPermission(GuildPermission.ManageRoles)]
+        [Command("listroles"), Summary("Lists joinable roles")]
+        private async Task ListRole()
+        {
+            await Context.Channel.SendMessageAsync("Joinable roles: ");
+        }
+
+
+
+    }
 }
