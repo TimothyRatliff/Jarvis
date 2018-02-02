a3d891f1643e0531a35bdc00e4b664b75bc35006
Timothy Ratliff <timothykratliff@gmail.com>
2018-02-02 09:36:31 -0600

    begin work on better logging

---
+++ Jarvis/Jarvis/addons/Logging/Logging.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Discord;
+using Discord.Audio;
+using Discord.Commands;
+
+namespace Jarvis.addons.Logging
+{
+    class Logging
+    {
+        public void Log(String msg)
+        {
+            Console.WriteLine($"{DateTime.Now.ToString()} {msg}");
+        }
+        
+    }
+}
