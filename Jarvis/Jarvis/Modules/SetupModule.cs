100c777a699d4b1f6c64d068d0b112e4a47ea7e4
Timothy Ratliff <timothykratliff@gmail.com>
2018-03-27 01:42:16 -0500

    begin work on guild-specific settings

---
+++ Jarvis/Jarvis/Modules/SetupModule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Modules;
+using Discord.Commands;
+using Discord.WebSocket;
+using Jarvis.addons.Logging;
+using Jarvis.addons.Settings;
+
+namespace Jarvis.Modules
+{
+    public class SetupModule : ModuleBase
+    {
+
+        //Embed color for InfoModule is builder.WithColor(Color.Black);
+        [RequireUserPermission(GuildPermission.ManageRoles)]
+        [RequireBotPermission(GuildPermission.ManageRoles)]
+        [Command("setup", RunMode = RunMode.Async), Summary("Begin setup of Jarvis")]
+        public async Task Setup()
+        {
+            Setter.SetterInstance.NewSetup(Context.Guild);
+
+            await Context.Channel.SendMessageAsync($"Started a settings entry for **{Context.Guild.Name}** :white_check_mark:");
+            Logger.LoggerInstance.Log("setup", Context.Guild, Context.Channel);
+        }
+    }
+}
---
+++ Jarvis/Jarvis/addons/Settings/Setter.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Discord;
+using Discord.Modules;
+using Discord.Commands;
+using Discord.WebSocket;
+using Jarvis.addons.Logging;
+using Newtonsoft.Json;
+
+namespace Jarvis.addons.Settings
+{
+    class Setter
+    {
+        private static Setter setter = new Setter();
+
+        //private FileStream outstrm;
+
+
+        public static Setter SetterInstance
+        {
+            get { return setter; }
+        }
+
+        public void NewSetup(IGuild guild)
+        {
+            using (FileStream fs = File.Open("./serverdb.json", FileMode.Append, FileAccess.Write))
+            using (StreamWriter sw = new StreamWriter(fs))
+            using (JsonWriter writer = new JsonTextWriter(sw))
+            {
+                writer.Formatting = Formatting.Indented;
+
+                //writer.WriteStartObject();
+                //writer.WritePropertyName("Guild Name");
+                //writer.WriteValue(guild.Name);
+                ////writer.WritePropertyName("PSU");
+                ////writer.WriteValue("500W");
+                ////writer.WritePropertyName("Drives");
+                ////writer.WriteStartArray();
+                ////writer.WriteValue("DVD read/writer");
+                ////writer.WriteComment("(broken)");
+                ////writer.WriteValue("500 gigabyte hard drive");
+                ////writer.WriteValue("200 gigabype hard drive");
+                ////writer.WriteEnd();
+                //writer.WriteEndObject();
+
+                //JsonSerializer serializer = new JsonSerializer();
+                //serializer.Serialize(writer, guild.Name);
+            }
+
+        }
+
+        //public async Boolean IsSetup(IGuild guild)
+        //{
+        //    using (FileStream fs = File.Open("./serverdb.json", FileMode.Append, FileAccess.Write))
+        //    using (StreamReader sr = new StreamReader(fs))
+        //    using (JsonReader reader = new JsonTextReader(sr)
+        //    {
+        //        reader.
+
+        //    }
+        //}
+    }
+}
