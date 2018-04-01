b5baf2b0837eb3cc31ad232df2b4934e8b128bb9
TimothyRatliff <timothykratliff@gmail.com>
2018-04-01 13:13:25 -0500

    starting from scratch on voice/audio

--- Jarvis/Jarvis/addons/Audio/AudioConfig.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Discord.Addons.SimpleAudio
-{
-    public class AudioConfig
-    {
-        public string FFMpegPath { get; set; }
-        public string MusicBasePath { get; set; }
-    }
-}
