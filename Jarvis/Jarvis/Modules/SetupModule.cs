9859c3e184e88acac42de6786db706ebcf7de523
TimothyRatliff <timothykratliff@gmail.com>
2018-04-01 00:15:24 -0500

    experimental v0.1.16b

--- Jarvis/Jarvis/Modules/SetupModule.cs
+++ Jarvis/Jarvis/Modules/SetupModule.cs
@@ -1,50 +1,50 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Discord;
-using Discord.Modules;
-using Discord.Commands;
-using Discord.WebSocket;
-using Jarvis.addons.Logging;
-using Jarvis.addons.Settings;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using System.Diagnostics;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using Discord;
+//using Discord.Modules;
+//using Discord.Commands;
+//using Discord.WebSocket;
+//using Jarvis.addons.Logging;
+//using Jarvis.addons.Settings;
 
-namespace Jarvis.Modules
-{
-    public class SetupModule : ModuleBase
-    {
+//namespace Jarvis.Modules
+//{
+//    public class SetupModule : ModuleBase
+//    {
 
-        //Embed color for SetupModule is builder.WithColor(Color.Black);
+//        //Embed color for SetupModule is builder.WithColor(Color.Black);
 
-        [RequireUserPermission(GuildPermission.ManageRoles)]
-        [RequireBotPermission(GuildPermission.ManageRoles)]
-        [Command("setup", RunMode = RunMode.Async), Summary("Begin setup of Jarvis")]
-        public async Task Setup()
-        {
-            Setter.SetterInstance.NewSetup(Context.Guild);
+//        [RequireUserPermission(GuildPermission.ManageRoles)]
+//        [RequireBotPermission(GuildPermission.ManageRoles)]
+//        [Command("setup", RunMode = RunMode.Async), Summary("Begin setup of Jarvis")]
+//        public async Task Setup()
+//        {
+//            Setter.SetterInstance.NewSetup(Context.Guild);
 
-            await Context.Channel.SendMessageAsync($"Created a settings entry for **{Context.Guild.Name}** :white_check_mark:");
-            Logger.LoggerInstance.Log("setup", Context.Guild, Context.Channel);
-        }
+//            await Context.Channel.SendMessageAsync($"Created a settings entry for **{Context.Guild.Name}** :white_check_mark:");
+//            Logger.LoggerInstance.Log("setup", Context.Guild, Context.Channel);
+//        }
 
-        [RequireUserPermission(GuildPermission.ManageRoles)]
-        [RequireBotPermission(GuildPermission.ManageRoles)]
-        [Command("issetup", RunMode = RunMode.Async), Summary("Begin setup of Jarvis")]
-        public async Task IsSetup()
-        {
+//        [RequireUserPermission(GuildPermission.ManageRoles)]
+//        [RequireBotPermission(GuildPermission.ManageRoles)]
+//        [Command("issetup", RunMode = RunMode.Async), Summary("Begin setup of Jarvis")]
+//        public async Task IsSetup()
+//        {
 
-            if (Setter.SetterInstance.IsSetup(Context.Guild) == true)
-            {
-                await Context.Channel.SendMessageAsync($"This server has been setup.");
-            }
-            else
-                await Context.Channel.SendMessageAsync($"This server has not been setup.");
+//            if (Setter.SetterInstance.IsSetup(Context.Guild) == true)
+//            {
+//                await Context.Channel.SendMessageAsync($"This server has been setup.");
+//            }
+//            else
+//                await Context.Channel.SendMessageAsync($"This server has not been setup.");
 
-            Logger.LoggerInstance.Log("issetup", Context.Guild, Context.Channel);
-        }
+//            Logger.LoggerInstance.Log("issetup", Context.Guild, Context.Channel);
+//        }
 
 
-    }
-}
+//    }
+//}
--- Jarvis/Jarvis/addons/Settings/Setter.cs
+++ Jarvis/Jarvis/addons/Settings/Setter.cs
@@ -1,100 +1,100 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using System.IO;
-using Discord;
-using Discord.Modules;
-using Discord.Commands;
-using Discord.WebSocket;
-using Jarvis.addons.Logging;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using System.Diagnostics;
+//using System.Linq;
+//using System.Threading.Tasks;
+//using System.IO;
+//using Discord;
+//using Discord.Modules;
+//using Discord.Commands;
+//using Discord.WebSocket;
+//using Jarvis.addons.Logging;
+//using Newtonsoft.Json;
+//using Newtonsoft.Json.Linq;
 
-namespace Jarvis.addons.Settings
-{
-    class Setter
-    {
-        private static Setter setter = new Setter();
+//namespace Jarvis.addons.Settings
+//{
+//    class Setter
+//    {
+//        private static Setter setter = new Setter();
 
-        //private FileStream outstrm;
-
-
-        public static Setter SetterInstance
-        {
-            get { return setter; }
-        }
+//        //private FileStream outstrm;
 
 
-        public class GuildSetting
-        {
-            public string name;
-            public string isSetup;
-        }
+//        public static Setter SetterInstance
+//        {
+//            get { return setter; }
+//        }
 
 
-        public void NewSetup(IGuild guild)
-        {
-            using (FileStream fs = File.Open("./serverdb.json", FileMode.Append, FileAccess.Write))
-            using (StreamWriter sw = new StreamWriter(fs))
-            using (JsonWriter writer = new JsonTextWriter(sw))
-            {
-                writer.Formatting = Formatting.Indented;
-
-                writer.WriteStartObject();
-                writer.WritePropertyName("Guild Name");
-                writer.WriteValue(guild.Name);
-                writer.WritePropertyName("isSetup");
-                writer.WriteValue(true);
-                ////writer.WritePropertyName("PSU");
-                ////writer.WriteValue("500W");
-                ////writer.WritePropertyName("Drives");
-                ////writer.WriteStartArray();
-                ////writer.WriteValue("DVD read/writer");
-                ////writer.WriteComment("(broken)");
-                ////writer.WriteValue("500 gigabyte hard drive");
-                ////writer.WriteValue("200 gigabype hard drive");
-                ////writer.WriteEnd();
-                writer.WriteEndObject();
+//        public class GuildSetting
+//        {
+//            public string name;
+//            public string isSetup;
+//        }
 
 
-                //JsonSerializer serializer = new JsonSerializer();
-                //serializer.Serialize(writer, guild.Name);
-            }
+//        public void NewSetup(IGuild guild)
+//        {
+//            using (FileStream fs = File.Open("./serverdb.json", FileMode.Append, FileAccess.Write))
+//            using (StreamWriter sw = new StreamWriter(fs))
+//            using (JsonWriter writer = new JsonTextWriter(sw))
+//            {
+//                writer.Formatting = Formatting.Indented;
 
-        }
+//                writer.WriteStartObject();
+//                writer.WritePropertyName("Guild Name");
+//                writer.WriteValue(guild.Name);
+//                writer.WritePropertyName("isSetup");
+//                writer.WriteValue(true);
+//                ////writer.WritePropertyName("PSU");
+//                ////writer.WriteValue("500W");
+//                ////writer.WritePropertyName("Drives");
+//                ////writer.WriteStartArray();
+//                ////writer.WriteValue("DVD read/writer");
+//                ////writer.WriteComment("(broken)");
+//                ////writer.WriteValue("500 gigabyte hard drive");
+//                ////writer.WriteValue("200 gigabype hard drive");
+//                ////writer.WriteEnd();
+//                writer.WriteEndObject();
+
+
+//                //JsonSerializer serializer = new JsonSerializer();
+//                //serializer.Serialize(writer, guild.Name);
+//            }
+
+//        }
 
 
         
 
-        public Boolean IsSetup(IGuild guild)
-        {
-            using (StreamReader file = new StreamReader("./serverdb.json"))
-            //using (JsonTextReader reader = new JsonTextReader(file))
-            {
-                //JObject serverdb = (JObject)JToken.ReadFrom(reader);
+//        public Boolean IsSetup(IGuild guild)
+//        {
+//            using (StreamReader file = new StreamReader("./serverdb.json"))
+//            //using (JsonTextReader reader = new JsonTextReader(file))
+//            {
+//                //JObject serverdb = (JObject)JToken.ReadFrom(reader);
 
-                string json = file.ReadToEnd();
-                Console.WriteLine(json);
-                var serverdb = JsonConvert.DeserializeObject<List<JObject>>(json);
-                Console.WriteLine(serverdb.First().GetValue("Guild Name").ToString());
-                //string guildname = (string)serverdb["Guild Name"][guild.Name];
-                //Console.WriteLine(serverdb.ToString());
-                //if (serverdb.GetValue("Guild Name").ToString() == guild.Name)
-                //    return true;
-                //return false;
+//                string json = file.ReadToEnd();
+//                Console.WriteLine(json);
+//                var serverdb = JsonConvert.DeserializeObject<List<JObject>>(json);
+//                Console.WriteLine(serverdb.First().GetValue("Guild Name").ToString());
+//                //string guildname = (string)serverdb["Guild Name"][guild.Name];
+//                //Console.WriteLine(serverdb.ToString());
+//                //if (serverdb.GetValue("Guild Name").ToString() == guild.Name)
+//                //    return true;
+//                //return false;
 
-                foreach (JObject obj in serverdb)
-                {
-                    if (obj.GetValue("Guild Name").ToString() == guild.Name)
-                        return true;
-                }
-                return false;
+//                foreach (JObject obj in serverdb)
+//                {
+//                    if (obj.GetValue("Guild Name").ToString() == guild.Name)
+//                        return true;
+//                }
+//                return false;
 
-            }
+//            }
 
-        }
-    }
-}
+//        }
+//    }
+//}
