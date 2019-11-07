b26b4c844c120240edb50aee7ac05ef7be1ce7e5
Timothy Ratliff <timothykratliff@gmail.com>
2019-11-06 20:29:18 -0600

    add testing project, both target .net core 2.2

---
+++ Jarvis.Bot.Test/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Jarvis.Bot.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
